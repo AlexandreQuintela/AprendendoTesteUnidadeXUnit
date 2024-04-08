ec73c8e9bee50a1a20f9aaf3c9441c7e318ca3b5
alexandrequintela <alexsquin@gmail.com>
2024-04-07 22:25:28 -0300

    Classe calculadora

---
+++ src/Demo/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo;
+
+public sealed class Calculadora
+{
+    public double Somar(double x, double y)
+    {
+        return x + y;
+    }
+
+    public int Dividir(int x, int y)
+    {
+        return x / y;
+    }
+}
